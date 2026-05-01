ca1da7c0dcf7cc02227cd34eba5cc6576757f55d
imada <imada@IMADA-AEROG11.cac-k.jp.local>
2026-05-01 13:44:59 +0900

    q2_advance question2のテストが成功になりませんでいた

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
@@ -1,10 +1,39 @@
 package q02_advanced.question02;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SystemMain02 {
 
 	public static void main(String[] args) {
 
 		//TODO ここから実装する
+		ConsoleReader cr = new ConsoleReader();
+
+		List<Member> members = new ArrayList<>();
+		members.add(Member.getInsyance(1, "Passw0rd", "Miura Manabu", 28, 2));
+		members.add(Member.getInsyance(2, "aaaAAA", "Sato Kensuke", 43, 1));
+
+		MemberManager.showAllMembers(members);
+		int id = 0;
+		String newpassword = null;
+
+		try {
+
+			System.out.print("input target id>> ");
+			id = cr.inputNumber();
+
+			System.out.println("input new password>>");
+			newpassword = cr.inputString();
+
+		} catch (IOException | NumberFormatException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return;
+		}
+		MemberManager.updatePasswaord(members, id, newpassword);
+		MemberManager.showAllMembers(members);
 
 	}
 
